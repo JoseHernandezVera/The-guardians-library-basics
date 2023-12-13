2dc80c9b0f5db8578078350bde87caf90be77b44
JoseHernandezVera <jh0280381@gmail.com>
2023-12-13 16:13:28 -0300

    Borro y cuanta nueva

--- TESTEO.cpp
+++ TESTEO.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
-#include <cstdlib>
 #include <ctime>
-#include <chrono>
+#include <cstdlib>
 #include <algorithm>
+#include <vector>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
-void swap(int &a, int &b) {
-    int temp = a;
-    a = b;
-    b = temp;
-}
-
+// Algoritmo de ordenamiento: Selection Sort
 void selectionSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         int minIndex = i;
@@ -24,6 +21,7 @@ void selectionSort(int arr[], int n) {
     }
 }
 
+// Algoritmo de ordenamiento: Bubble Sort
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
@@ -34,6 +32,7 @@ void bubbleSort(int arr[], int n) {
     }
 }
 
+// Algoritmo de ordenamiento: Insertion Sort
 void insertionSort(int arr[], int n) {
     for (int i = 1; i < n; i++) {
         int key = arr[i];
@@ -41,59 +40,261 @@ void insertionSort(int arr[], int n) {
 
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
-            j = j - 1;
+            j--;
         }
+
         arr[j + 1] = key;
     }
 }
 
-double medirTiempo(void (*algoritmo)(int[], int), int arr[], int tamano) {
-    auto inicio = chrono::high_resolution_clock::now();
-    algoritmo(arr, tamano);
-    auto fin = chrono::high_resolution_clock::now();
-    return chrono::duration<double, milli>(fin - inicio).count() / 1000.0;
+// Algoritmo de ordenamiento: Shell Sort
+void shellSort(int arr[], int n) {
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n; i++) {
+            int temp = arr[i];
+            int j;
+
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                arr[j] = arr[j - gap];
+            }
+
+            arr[j] = temp;
+        }
+    }
 }
 
-void mostrarTiempo(string nombre, double tiempo) {
-    cout << "El algoritmo de " << nombre << " se demoro " << tiempo << " segundos en ejecutarse." << endl;
+// Algoritmo de ordenamiento: Merge Sort
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    vector<int> L(n1), R(n2);
+
+    for (int i = 0; i < n1; i++) {
+        L[i] = arr[l + i];
+    }
+
+    for (int j = 0; j < n2; j++) {
+        R[j] = arr[m + 1 + j];
+    }
+
+    int i = 0;
+    int j = 0;
+    int k = l;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+// Algoritmo de ordenamiento: Quick Sort
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+// Algoritmo de ordenamiento: Heap Sort
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n) {
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        swap(arr[0], arr[i]);
+
+        heapify(arr, i, 0);
+    }
+}
+
+struct AlgorithmInfo {
+    string name;
+    double duration;
+};
+
+// Sobrecarga para funciones de ordenamiento que toman dos argumentos
+void runSortingAlgorithm(int arr[], int size, void (*sortingFunction)(int[], int), const string& algorithmName, vector<AlgorithmInfo>& algorithms) {
+    int* arrCopy = new int[size];
+    copy(arr, arr + size, arrCopy);
+
+    auto start = high_resolution_clock::now();
+    sortingFunction(arrCopy, size);
+    auto stop = high_resolution_clock::now();
+    double duration = duration_cast<milliseconds>(stop - start).count() / 1000.0;
+
+    cout << algorithmName << " tiempo de ejecucion: " << duration << " segundos\n";
+    algorithms.push_back({algorithmName, duration});
+
+    delete[] arrCopy;
+}
+
+// Sobrecarga para funciones de ordenamiento que toman tres argumentos
+void runSortingAlgorithm(int arr[], int size, void (*sortingFunction)(int[], int, int), const string& algorithmName, vector<AlgorithmInfo>& algorithms) {
+    int* arrCopy = new int[size];
+    copy(arr, arr + size, arrCopy);
+
+    auto start = high_resolution_clock::now();
+    sortingFunction(arrCopy, 0, size - 1);
+    auto stop = high_resolution_clock::now();
+    double duration = duration_cast<milliseconds>(stop - start).count() / 1000.0;
+
+    cout << algorithmName << " tiempo de ejecucion: " << duration << " segundos\n";
+    algorithms.push_back({algorithmName, duration});
+
+    delete[] arrCopy;
+}
+
+// Función para encontrar el algoritmo más eficiente
+AlgorithmInfo findMostEfficientAlgorithm(const vector<AlgorithmInfo>& algorithms) {
+    auto minAlgorithm = min_element(algorithms.begin(), algorithms.end(),
+        [](const AlgorithmInfo& a, const AlgorithmInfo& b) {
+            return a.duration < b.duration;
+        });
+
+    return *minAlgorithm;
 }
 
 int main() {
+    // Generar un número aleatorio entre 100,000 y 110,000 para determinar el tamaño del arreglo
+    int size = rand() % 10001 + 100000;
+
+    // Crear el arreglo con el tamaño aleatorio
+    int* arr = new int[size];
+
     srand(time(0));
-    int tamano = rand() % 11001 + 100000;
-
-    cout << "El tamano es de " << tamano << endl;
-
-    // Crear el arreglo de números aleatorios en el rango de 100000 a 110000 con posibilidad de repetición
-    int *originalArr = new int[tamano];
-    for (int i = 0; i < tamano; i++) {
-        originalArr[i] = rand() % 100001;
+    // Llenar el arreglo con números aleatorios
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 110001;  // Puedes ajustar el rango según tus necesidades
     }
 
-    // Crear copias del arreglo original para cada algoritmo
-    int *arrSelection = new int[tamano];
-    int *arrBubble = new int[tamano];
-    int *arrInsertion = new int[tamano];
+    // Almacenar información de cada algoritmo
+    vector<AlgorithmInfo> algorithms;
 
-    // Copiar el contenido del arreglo original a las copias
-    std::copy(originalArr, originalArr + tamano, arrSelection);
-    std::copy(originalArr, originalArr + tamano, arrBubble);
-    std::copy(originalArr, originalArr + tamano, arrInsertion);
+    int choice;
+    int choice2;
+    
+    cout << "Menu\n1. Cola de espera ";
+    cout << "\nSelecciona una opcion: ";
+    cin >> choice;
 
-    // Medicion de tiempos y mostrar resultados
-    double tiempo_selection = medirTiempo(selectionSort, arrSelection, tamano);
-    double tiempo_bubble = medirTiempo(bubbleSort, arrBubble, tamano);
-    double tiempo_insertion = medirTiempo(insertionSort, arrInsertion, tamano);
+    switch (choice) {
+        case 1:
 
-    mostrarTiempo("selection sort", tiempo_selection);
-    mostrarTiempo("bubble sort", tiempo_bubble);
-    mostrarTiempo("insertion sort", tiempo_insertion);
+        	cout << "\nEstas son las carreras:\n";
+        	cout << "1. Aleatorios con numeros repetidos\n2. Aleatorios sin numeros repetidos\n3. Ordenado\n4. Inversamente ordenado";
+        	cout << "Escoge una carrera: ";
+        	cin >> choice2;
+        	
+        	switch (choice2) {
+        		case 1:
+        		
+        		cout << "El tamano del arreglo es " << size << endl;
 
-    // Liberar la memoria de los arreglos
-    delete[] originalArr;
-    delete[] arrSelection;
-    delete[] arrBubble;
-    delete[] arrInsertion;
+	            // Selection Sort
+	            runSortingAlgorithm(arr, size, selectionSort, "Selection Sort", algorithms);
+	
+	            // Bubble Sort
+	            runSortingAlgorithm(arr, size, bubbleSort, "Bubble Sort", algorithms);
+	
+	            // Insertion Sort
+	            runSortingAlgorithm(arr, size, insertionSort, "Insertion Sort", algorithms);
+	
+	            // Shell Sort
+	            runSortingAlgorithm(arr, size, shellSort, "Shell Sort", algorithms);
+	
+	            // Merge Sort
+	            runSortingAlgorithm(arr, size, mergeSort, "Merge Sort", algorithms);
+	
+	            // Quick Sort
+	            runSortingAlgorithm(arr, size, quickSort, "Quick Sort", algorithms);
+	
+	            // Heap Sort
+	            runSortingAlgorithm(arr, size, heapSort, "Heap Sort", algorithms);
+	
+	            // Encontrar el algoritmo que se demoró menos
+	            AlgorithmInfo minAlgorithm = findMostEfficientAlgorithm(algorithms);
+	
+	            // Imprimir resultados
+	            cout << "\nEl algoritmo que se demoro menos fue: " << minAlgorithm.name;
+	            cout << " se demoro " << minAlgorithm.duration << " segundos en ejecutarse.\n";
+	
+	            // Liberar la memoria de los arreglos dinámicos
+	            delete[] arr;
+	            
+	            break;
+        		
+			}
+
+
+            break;
+    }
 
     return 0;
 }
--- The guardians library-basics.cpp
+++ The guardians library-basics.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
-#include <cstdlib>
 #include <ctime>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
 #include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
-void swap(int &a, int &b) {
-    int temp = a;
-    a = b;
-    b = temp;
-}
-
+// Algoritmo de ordenamiento: Selection Sort
 void selectionSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         int minIndex = i;
@@ -23,6 +21,7 @@ void selectionSort(int arr[], int n) {
     }
 }
 
+// Algoritmo de ordenamiento: Bubble Sort
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
@@ -33,6 +32,7 @@ void bubbleSort(int arr[], int n) {
     }
 }
 
+// Algoritmo de ordenamiento: Insertion Sort
 void insertionSort(int arr[], int n) {
     for (int i = 1; i < n; i++) {
         int key = arr[i];
@@ -40,50 +40,261 @@ void insertionSort(int arr[], int n) {
 
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
-            j = j - 1;
+            j--;
         }
+
         arr[j + 1] = key;
     }
 }
 
-int main() {
-    srand(time(0));
-    int tamano = rand() % 11001 + 100000;
+// Algoritmo de ordenamiento: Shell Sort
+void shellSort(int arr[], int n) {
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n; i++) {
+            int temp = arr[i];
+            int j;
 
-    cout << "El tamano es de " << tamano << endl;
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                arr[j] = arr[j - gap];
+            }
 
-    // Crear el arreglo de números aleatorios en el rango de 100000 a 110000 con posibilidad de repetición
-    int *arr = new int[tamano];
-    for (int i = 0; i < tamano; i++) {
-        arr[i] = rand() % 100001;
+            arr[j] = temp;
+        }
+    }
+}
+
+// Algoritmo de ordenamiento: Merge Sort
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    vector<int> L(n1), R(n2);
+
+    for (int i = 0; i < n1; i++) {
+        L[i] = arr[l + i];
     }
 
-    // Medicion de tiempos
-    double tiempo_selection = 0.0;
-    double tiempo_bubble = 0.0;
-    double tiempo_insertion = 0.0;
+    for (int j = 0; j < n2; j++) {
+        R[j] = arr[m + 1 + j];
+    }
 
-    auto inicio_selection = chrono::high_resolution_clock::now();
-    selectionSort(arr, tamano);
-    auto fin_selection = chrono::high_resolution_clock::now();
-    tiempo_selection = chrono::duration<double, milli>(fin_selection - inicio_selection).count();
+    int i = 0;
+    int j = 0;
+    int k = l;
 
-    auto inicio_bubble = chrono::high_resolution_clock::now();
-    bubbleSort(arr, tamano);
-    auto fin_bubble = chrono::high_resolution_clock::now();
-    tiempo_bubble = chrono::duration<double, milli>(fin_bubble - inicio_bubble).count();
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
 
-    auto inicio_insertion = chrono::high_resolution_clock::now();
-    insertionSort(arr, tamano);
-    auto fin_insertion = chrono::high_resolution_clock::now();
-    tiempo_insertion = chrono::duration<double, milli>(fin_insertion - inicio_insertion).count();
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
 
-    cout << "El algoritmo de selection sort se demoro " << tiempo_selection << " milisegundos en ejecutarse." << endl;
-    cout << "El algoritmo de bubble sort se demoro " << tiempo_bubble << " milisegundos en ejecutarse." << endl;
-    cout << "El algoritmo de insertion sort se demoro " << tiempo_insertion << " milisegundos en ejecutarse." << endl;
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
 
-    // Liberar la memoria del arreglo
-    delete[] arr;
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+// Algoritmo de ordenamiento: Quick Sort
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+// Algoritmo de ordenamiento: Heap Sort
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n) {
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        swap(arr[0], arr[i]);
+
+        heapify(arr, i, 0);
+    }
+}
+
+struct AlgorithmInfo {
+    string name;
+    double duration;
+};
+
+// Sobrecarga para funciones de ordenamiento que toman dos argumentos
+void runSortingAlgorithm(int arr[], int size, void (*sortingFunction)(int[], int), const string& algorithmName, vector<AlgorithmInfo>& algorithms) {
+    int* arrCopy = new int[size];
+    copy(arr, arr + size, arrCopy);
+
+    auto start = high_resolution_clock::now();
+    sortingFunction(arrCopy, size);
+    auto stop = high_resolution_clock::now();
+    double duration = duration_cast<milliseconds>(stop - start).count() / 1000.0;
+
+    cout << algorithmName << " tiempo de ejecucion: " << duration << " segundos\n";
+    algorithms.push_back({algorithmName, duration});
+
+    delete[] arrCopy;
+}
+
+// Sobrecarga para funciones de ordenamiento que toman tres argumentos
+void runSortingAlgorithm(int arr[], int size, void (*sortingFunction)(int[], int, int), const string& algorithmName, vector<AlgorithmInfo>& algorithms) {
+    int* arrCopy = new int[size];
+    copy(arr, arr + size, arrCopy);
+
+    auto start = high_resolution_clock::now();
+    sortingFunction(arrCopy, 0, size - 1);
+    auto stop = high_resolution_clock::now();
+    double duration = duration_cast<milliseconds>(stop - start).count() / 1000.0;
+
+    cout << algorithmName << " tiempo de ejecucion: " << duration << " segundos\n";
+    algorithms.push_back({algorithmName, duration});
+
+    delete[] arrCopy;
+}
+
+// Función para encontrar el algoritmo más eficiente
+AlgorithmInfo findMostEfficientAlgorithm(const vector<AlgorithmInfo>& algorithms) {
+    auto minAlgorithm = min_element(algorithms.begin(), algorithms.end(),
+        [](const AlgorithmInfo& a, const AlgorithmInfo& b) {
+            return a.duration < b.duration;
+        });
+
+    return *minAlgorithm;
+}
+
+int main() {
+    // Generar un número aleatorio entre 100,000 y 110,000 para determinar el tamaño del arreglo
+    int size = rand() % 10001 + 100000;
+
+    // Crear el arreglo con el tamaño aleatorio
+    int* arr = new int[size];
+
+    srand(time(0));
+    // Llenar el arreglo con números aleatorios
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % 110001;  // Puedes ajustar el rango según tus necesidades
+    }
+
+    // Almacenar información de cada algoritmo
+    vector<AlgorithmInfo> algorithms;
+
+    int choice;
+    int choice2;
+    
+    cout << "Menu\n1. Cola de espera ";
+    cout << "\nSelecciona una opcion: ";
+    cin >> choice;
+
+    switch (choice) {
+        case 1:
+
+        	cout << "\nEstas son las carreras:\n";
+        	cout << "1. Aleatorios con numeros repetidos\n2. Aleatorios sin numeros repetidos\n3. Ordenado\n4. Inversamente ordenado";
+        	cout << "Escoge una carrera: ";
+        	cin >> choice2;
+        	
+        	switch (choice2) {
+        		case 1:
+        		
+        		cout << "El tamano del arreglo es " << size << endl;
+
+	            // Selection Sort
+	            runSortingAlgorithm(arr, size, selectionSort, "Selection Sort", algorithms);
+	
+	            // Bubble Sort
+	            runSortingAlgorithm(arr, size, bubbleSort, "Bubble Sort", algorithms);
+	
+	            // Insertion Sort
+	            runSortingAlgorithm(arr, size, insertionSort, "Insertion Sort", algorithms);
+	
+	            // Shell Sort
+	            runSortingAlgorithm(arr, size, shellSort, "Shell Sort", algorithms);
+	
+	            // Merge Sort
+	            runSortingAlgorithm(arr, size, mergeSort, "Merge Sort", algorithms);
+	
+	            // Quick Sort
+	            runSortingAlgorithm(arr, size, quickSort, "Quick Sort", algorithms);
+	
+	            // Heap Sort
+	            runSortingAlgorithm(arr, size, heapSort, "Heap Sort", algorithms);
+	
+	            // Encontrar el algoritmo que se demoró menos
+	            AlgorithmInfo minAlgorithm = findMostEfficientAlgorithm(algorithms);
+	
+	            // Imprimir resultados
+	            cout << "\nEl algoritmo que se demoro menos fue: " << minAlgorithm.name;
+	            cout << " se demoro " << minAlgorithm.duration << " segundos en ejecutarse.\n";
+	
+	            // Liberar la memoria de los arreglos dinámicos
+	            delete[] arr;
+	            
+	            break;
+        		
+			}
+
+
+            break;
+    }
 
     return 0;
 }
